6fed8f17d1c4fa3e71b6a392f3c799348744c65f
LiN <alexlamnh@gmail.com>
2018-10-27 00:59:03 -0300

    twitter.py - print by user and hashtag

--- tweepylib/twitter.py
+++ tweepylib/twitter.py
@@ -3,14 +3,38 @@ import tweepy
 
 auth = tweepy.OAuthHandler(tk.CONSUMER_KEY, tk.CONSUMER_SECRET)
 auth.set_access_token(tk.TOKEN_ACCESS, tk.TOKEN_SECRET)
-
 api = tweepy.API(auth)
 
-user = 'jairbolsonaro'
+user = 'marcosmion'
+hashtag = '#masterchef'
 count = 10
 
-tweets = api.user_timeline(id=user, count=count, tweet_mode='extended')
 
-for tweet in tweets:
-    print(tweet.full_text)
-    print('---------')
+def get_tweets(user, count):
+    results = api.user_timeline(id=user, count=count, tweet_mode='extended')
+    tweets = []
+    for tweet in results:
+        tweets.append(tweet.full_text)
+        print(tweet.user.screen_name)
+        print(tweet.created_at)
+        print(tweet.full_text)
+        print('---')
+    return tweets
+
+
+def get_hashtags(hashtag, count):
+    results = api.search(q=hashtag, count=count, tweet_mode='extended')
+    tweets = []
+    for tweet in results:
+        tweets.append(tweet)
+        print(tweet.user.screen_name)
+        print(tweet.created_at)
+        print(tweet.full_text)
+        print('---')
+    return tweets
+
+
+get_tweets(user, count)
+
+get_hashtags(hashtag, count)
+
