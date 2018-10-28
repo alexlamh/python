8ae2e915554a227910f42197bcae8070e0256e01
LiN <alexlamnh@gmail.com>
2018-10-28 02:54:56 -0300

    code fix

--- crawler_investing/crypto.py
+++ crawler_investing/crypto.py
@@ -2,30 +2,34 @@ import helpers.selenium_handler as h
 import csv
 import datetime as date
 import pytz
+import os
 
-URL = 'https://m.investing.com/crypto/'
 
 def timestamp():
     tz = pytz.timezone('America/Sao_Paulo')
     timestamp = date.datetime.now(tz).strftime("%Y-%m-%d %H:%M")
     return timestamp
 
-def csv_write(param1, param2, param3, param4):
-    with open('./downloads/crypto2.csv', 'a', newline='') as saida:
-        escrever = csv.writer(saida, delimiter=';')
-        escrever.writerow([param1, param2, param3, param4])
-    return print('{} gravado'.format(param2))
 
+URL = 'https://m.investing.com/crypto/'
+path = '/home/lin/Documents/crypto.csv'
 
 if __name__ == '__main__':
     h.get(URL)
 
-    coin_rank = h.xpathList('/html/body/div[1]/div[1]/section/div/div/div/table/tbody/tr/td[1]')
     coin_name = h.xpathList('/html/body/div[1]/div[1]/section/div/div/div/table/tbody/tr/td[2]')
     coin_price = h.xpathList('/html/body/div[1]/div[1]/section/div/div/div/table/tbody/tr/td[3]')
 
-    for i in coin_price:
-        print(i.text)
+    for i in range(0,len(coin_name)):
+        name = coin_name[i].text
+        coin = coin_price[i].text
+        coin = coin.replace(',','')  # removendo as vírgulas dos valores
 
-
-
+        # gravando em csv
+        file_exists = os.path.isfile(path)
+        with open(path, 'a', newline='') as saida:
+            headers = ['rank', 'coin', 'usd', 'timestamp']
+            writer = csv.DictWriter(saida, delimiter=';', lineterminator='\n', fieldnames=headers)
+            if not file_exists:
+                writer.writeheader()
+            writer.writerow({'rank': i+1, 'coin': name, 'usd': coin, 'timestamp': timestamp()})
--- helpers/selenium_handler.py
+++ helpers/selenium_handler.py
@@ -1,29 +1,27 @@
-import selenium
 from selenium import webdriver
 
 drv = webdriver.Chrome('/home/lin/python/chromedriver')
-# drv = webdriver.PhantomJS('D:\Dropbox\python\helpers\phantomjs.exe')
 
 def get(url):
-    drv.get(url)
+    return drv.get(url)
 
 def css(param):
-    drv.find_element_by_css_selector(param)
+    return drv.find_element_by_css_selector(param)
 
 def cssList(param):
-    drv.find_elements_by_css_selector(param)
+    return drv.find_elements_by_css_selector(param)
 
 def cssClass(param,classe):
-    drv.find_element_by_css_selector(param).find_element_by_class_name(classe)
+    return drv.find_element_by_css_selector(param).find_element_by_class_name(classe)
 
 def cssClassAttr(param,classe,atributo):
-    drv.find_element_by_css_selector(param).find_element_by_class_name(classe).get_attribute(atributo)
+    return drv.find_element_by_css_selector(param).find_element_by_class_name(classe).get_attribute(atributo)
 
 def cssAttr(param,atributo):
-    drv.find_element_by_css_selector(param).get_attribute(atributo)
+    return drv.find_element_by_css_selector(param).get_attribute(atributo)
 
 def xpath(param):
-    drv.find_element_by_xpath(param)
+    return drv.find_element_by_xpath(param)
 
 def xpathList(param):
-    drv.find_elements_by_xpath(param)
+    return drv.find_elements_by_xpath(param)
